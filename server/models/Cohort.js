5cf91ca730f8a11d66c71b93567bcb2f227346a3
gerarmc <gerardomelendezpro@gmail.com>
2024-01-05 13:51:47 -0500

    Crud operation complete

--- server/models/Cohort.js
+++ server/models/Cohort.js
@@ -1,16 +1,16 @@
 const { model, Schema } = require("mongoose")
 
 const cohortSchema = new Schema({
-    inProgress: Boolean,
-    cohortSlug: String,
-    cohortName: String,
-    program: String,
-    campus: String,
-    startDate: String,
-    endDate: String,
-    programManager: String,
-    leadTeacher: String,
-    totalHours: Number,
+    inProgress: { type: Boolean, required: true },
+    cohortSlug: { type: String, required: true },
+    cohortName: { type: String, required: true },
+    program: { type: String, required: true },
+    campus: { type: String, required: true },
+    startDate: { type: String, required: true },
+    endDate: { type: String, required: true },
+    programManager: { type: String, required: true },
+    leadTeacher: { type: String, required: true },
+    totalHours: { type: Number, min: 0 },
   });
 
 module.exports = model("Cohort", cohortSchema);
--- server/models/Student.js
+++ server/models/Student.js
@@ -1,17 +1,16 @@
 const { model, Schema } = require("mongoose")
 
 const studentSchema = new Schema({
-    firstName: String,
-    lastName: String,
-    email: String,
-    phone: String,
-    linkedinUrl: String,
-    languages: [String
-    ],
-    program: String,
-    background: String,
-    image: String,
-    projects: [String],
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true},
+    phone: { type: String, required: true, unique: true},
+    linkedinUrl: { type: String, required: true, unique: true},
+    languages: {type: [String], required: true},
+    program: { type: String, required: true },
+    background: { type: String, required: true },
+    image: { type: String, default: "https://i.imgur.com/r8bo8u7.png" },
+    projects: { type: [String] },
     cohort: { type: Schema.Types.ObjectId, ref: "Cohort"}
   });
 
